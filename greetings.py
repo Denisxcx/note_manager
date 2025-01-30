d54ea1a4773afc415bd52d32056862aa0715fa4f
Denis <studentq00@mail.ru>
2025-01-31 01:41:38 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,8 @@
+
+username = 'Denis'
+title = 'Lesson zero'
+content = 'Gite zero'
+status = 'play'
+created_date = '10-11-2025'
+issue_date = '14-11-2025'
+print('Имя пользователя', username,'\n' 'Заголовок заметки', title,'\n' 'Описание заметки', content,'\n' 'Статус заметки', status,'\n', created_date,'\n', issue_date)
