d922b94291496aba75d9a32bc1adc237e7efdd37
Denis <studentq00@mail.ru>
2025-01-31 02:54:18 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,8 +1,8 @@
 
-username = 'Denis'
-title = 'Lesson zero'
-content = 'Gite zero'
-status = 'play'
+username = 'Имя'
+title = 'Заголовок'
+content = 'Заметка'
+status = 'Готов'
 created_date = '10-11-2025'
 issue_date = '14-11-2025'
 print('Имя пользователя', username,'\n' 'Заголовок заметки', title,'\n' 'Описание заметки', content,'\n' 'Статус заметки', status,'\n', created_date,'\n', issue_date)
